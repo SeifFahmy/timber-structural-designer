5675125160333e4571e3c439f4661c96732757b8
Seif (Cundall) <seifehab.bl4@gmail.com>
2025-01-23 16:31:17 +0000

    implemented react router with initial homepage

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,20 @@
 import { createRoot } from "react-dom/client";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
-const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+import Home from "./routes/home";
+
+const App = () => {
+    return (
+        <div>
+            Navbar
+            <HashRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+            </HashRouter>
+        </div>
+    );
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
