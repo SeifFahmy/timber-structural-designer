d74f050be6f713b336cbbbf2f913a075d32fcfb5
Seif (Cundall) <seifehab.bl4@gmail.com>
2025-01-24 11:26:52 +0000

    Implement placeholder robot API C# executable and connected it to Electron. Script gets the model's file name

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,15 +1,29 @@
+import { Button, Center, Stack, Text } from "@mantine/core";
+import { useState } from "react";
 import styles from "./Home.module.css";
-import { Button, Center, Container, Stack, Text } from "@mantine/core";
 
 const Home = () => {
+    const [result, setResult] = useState("");
+
+    const handleRobotImport = async () => {
+        try {
+            // Call the C# executable via Electron's main process
+            const robotModel = await (window as any).api.robotImport();
+            setResult(`Result: ${robotModel}`);
+        } catch (error) {
+            setResult(`Error: ${error}`);
+        }
+    };
+
     return (
         <Center h="100%">
             <Stack className={styles.root}>
                 <Text>
-                    Import the loads on the timber structure from Robot using the
-                    button below to get started.
+                    Import the loads on the timber structure from Robot using
+                    the button below to get started.
                 </Text>
-                <Button>Import Robot Loads</Button>
+                <Button onClick={handleRobotImport}>Import Robot Loads</Button>
+                {result && <Text>{result}</Text>}
             </Stack>
         </Center>
     );
--- webpack.main.config.ts
+++ webpack.main.config.ts
@@ -1,20 +1,23 @@
-import type { Configuration } from 'webpack';
-
-import { rules } from './webpack.rules';
-import { plugins } from './webpack.plugins';
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import type { Configuration } from "webpack";
+import { rules } from "./webpack.rules";
 
 export const mainConfig: Configuration = {
-  /**
-   * This is the main entry point for your application, it's the first file
-   * that runs in the main process.
-   */
-  entry: './src/index.ts',
-  // Put your normal webpack config below here
-  module: {
-    rules,
-  },
-  plugins,
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
-  },
+    /**
+     * This is the main entry point for your application, it's the first file
+     * that runs in the main process.
+     */
+    entry: "./src/index.ts",
+    // Put your normal webpack config below here
+    module: {
+        rules,
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{ from: "static", to: "static" }],
+        }),
+    ],
+    resolve: {
+        extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
+    },
 };
