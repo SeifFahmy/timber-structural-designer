6ca7c848df7779d6ff6f6d96bbdca6bcfb41ee9f
Seif (Cundall) <seifehab.bl4@gmail.com>
2025-02-04 10:46:12 +0000

    implemented initial ResultsTable component with a modal for each row

--- src/routes/results/Results.tsx
+++ src/routes/results/Results.tsx
@@ -11,6 +11,8 @@ import {
 } from "@mantine/core";
 import styles from "./Results.module.css";
 import { TeddsData } from "../../hooks/useTeddsStore";
+import { useDisclosure } from "@mantine/hooks";
+import ResultsTable from "./ResultsTable";
 
 const overviewData = [
     { title: "Max utilisation", content: 0.8 },
@@ -19,48 +21,10 @@ const overviewData = [
     { title: "Max section", content: "360x1000" },
 ];
 
-// id: number;
-// width: number;
-// depth: number;
-// result: number;
-// designMessage: string;
-// util: number;
-// material: string;
-// strength: string;
-// outputRtf: string;
-
-const elements = [
-    { id: 6, section: 12.011, result: "C", util: "Carbon" },
-    { id: 7, section: 14.007, result: "N", util: "Nitrogen" },
-    { id: 39, section: 88.906, result: "Y", util: "Yttrium" },
-    { id: 56, section: 137.33, result: "Ba", util: "Barium" },
-    { id: 58, section: 140.12, result: "Ce", util: "Cerium" },
-    { id: 58, section: 140.12, result: "Ce", util: "Cerium" },
-    { id: 58, section: 140.12, result: "Ce", util: "Cerium" },
-    { id: 58, section: 140.12, result: "Ce", util: "Cerium" },
-    { id: 58, section: 140.12, result: "Ce", util: "Cerium" },
-    { id: 58, section: 140.12, result: "Ce", util: "Cerium" },
-    { id: 58, section: 140.12, result: "Ce", util: "Cerium" },
-    { id: 58, section: 140.12, result: "Ce", util: "Cerium" },
-];
-
 const Results = () => {
     const handleRobotUpdate = () => {
         console.log("updated robot");
     };
-
-    const showMemberModal = (data: any) => {
-        console.log("showed modal");
-    };
-
-    const rows = elements.map((member) => (
-        <Table.Tr key={member.id} onClick={() => showMemberModal(member)}>
-            <Table.Td>{member.id}</Table.Td>
-            <Table.Td>{member.section}</Table.Td>
-            <Table.Td>{member.result}</Table.Td>
-            <Table.Td>{member.util}</Table.Td>
-        </Table.Tr>
-    ));
 
     return (
         <Center h="100%">
@@ -112,33 +76,7 @@ const Results = () => {
                         given below. Click on a member to inspect its results in
                         more detail.
                     </Text>
-                    <Table.ScrollContainer minWidth={500} h="13rem">
-                        <Table stickyHeader>
-                            <Table.Thead bg="var(--mantine-color-gray-2)">
-                                <Table.Tr className={styles.tableHeader}>
-                                    <Table.Th
-                                        style={{
-                                            borderRadius:
-                                                "var(--mantine-radius-md) 0 0 0",
-                                        }}
-                                    >
-                                        ID
-                                    </Table.Th>
-                                    <Table.Th>Section</Table.Th>
-                                    <Table.Th>Result</Table.Th>
-                                    <Table.Th
-                                        style={{
-                                            borderRadius:
-                                                "0 var(--mantine-radius-md) 0 0",
-                                        }}
-                                    >
-                                        Utilisation
-                                    </Table.Th>
-                                </Table.Tr>
-                            </Table.Thead>
-                            <Table.Tbody>{rows}</Table.Tbody>
-                        </Table>
-                    </Table.ScrollContainer>
+                    <ResultsTable />
                 </Stack>
             </Stack>
         </Center>
