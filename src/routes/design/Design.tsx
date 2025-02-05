34d947954b5617da04e1893f009f190cdca8395e
Seif (Cundall) <seifehab.bl4@gmail.com>
2025-02-05 17:36:28 +0000

    tedds store now saves tedds design results

--- src/routes/design/Design.tsx
+++ src/routes/design/Design.tsx
@@ -26,6 +26,19 @@ const robotData = [
 const Design = () => {
     const [errorMessage, setErrorMessage] = useState("");
     const [parentWindowName, setWindowName] = useState("");
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+    const updateTeddsData = useTeddsStore((state) => state.updateTeddsData);
+
+    const numMembers = robotData.length;
+    let totalTimeEstimate = "";
+    const timeEstimatePerDesign = 10;
+    if (numMembers * timeEstimatePerDesign < 60) {
+        totalTimeEstimate = `${numMembers * timeEstimatePerDesign}s`;
+    } else {
+        totalTimeEstimate = `${
+            Math.round(numMembers * timeEstimatePerDesign * 100) / 100
+        }min`;
+    }
 
     useEffect(() => {
         (async () => {
@@ -35,31 +48,38 @@ const Design = () => {
     }, []);
 
     const handleTeddsDesign = async () => {
+        setButtonDisabled(true);
         try {
             // Call the C# executable via Electron's main process
             const teddsDesign = await (window as any).api.teddsDesign(
                 parentWindowName,
                 JSON.stringify(robotData)
             );
-            useTeddsStore((state) => state.updateTeddsData(teddsDesign));
+            const teddsDesignJson = JSON.parse(teddsDesign);
+            updateTeddsData(teddsDesignJson);
         } catch (error) {
             setErrorMessage(
-                `Something went wrong. Please make sure Robot is open and contains timber elements, then try again. 
+                `Something went wrong. Please make sure Tekla Tedds is installed, then try again. 
                 If the problem persists, please contact me through the Contact page.`
             );
         }
+        setButtonDisabled(false);
     };
+
     return (
         <Center h="100%">
             <Stack className="mainContentContainer">
                 <Text>
                     Send the Robot model data to design in Tedds by clicking the
-                    button below.
+                    button below. There are {numMembers} members to design, so
+                    it's expected this will take {totalTimeEstimate} to
+                    complete.
                 </Text>
                 <Button
                     onClick={handleTeddsDesign}
                     variant="filled"
                     color="teal"
+                    disabled={buttonDisabled}
                 >
                     Design Structure
                 </Button>
